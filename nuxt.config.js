6740e986ac35ed537e8df3fcbd7c87c3f4e6eeac
Irnovi Albaweny <huiralb@gmail.com>
2018-09-02 14:22:27 +0700

    install bootstrap-vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,6 @@ const laravelNuxt = require("laravel-nuxt")
 
 module.exports = laravelNuxt({
     // Options such as mode, srcDir and generate.dir are already handled for you.
-    modules: [],
+    modules: ["bootstrap-vue/nuxt"],
     plugins: []
 })
